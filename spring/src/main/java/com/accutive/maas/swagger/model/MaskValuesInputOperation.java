b05992a11560e444976de3707fab610789877d61
SwaggerHub <SwaggerHub@smartbear.com>
2020-07-29 23:00:56 +0000

    Revise maskvalues endpoint to move all operation elements to request body

---
+++ spring/src/main/java/com/accutive/maas/swagger/model/MaskValuesInputOperation.java
@@ -0,0 +1,114 @@
+package com.accutive.maas.swagger.model;
+
+import java.util.Objects;
+import com.accutive.maas.swagger.model.MaskValuesInputOperationParameters;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * MaskValuesInputOperation
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-07-29T23:00:54.754Z[GMT]")
+public class MaskValuesInputOperation   {
+  @JsonProperty("id")
+  private Long id = null;
+
+  @JsonProperty("parameters")
+  @Valid
+  private List<MaskValuesInputOperationParameters> parameters = null;
+
+  public MaskValuesInputOperation id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+  **/
+  @ApiModelProperty(example = "23", value = "")
+  
+    public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public MaskValuesInputOperation parameters(List<MaskValuesInputOperationParameters> parameters) {
+    this.parameters = parameters;
+    return this;
+  }
+
+  public MaskValuesInputOperation addParametersItem(MaskValuesInputOperationParameters parametersItem) {
+    if (this.parameters == null) {
+      this.parameters = new ArrayList<MaskValuesInputOperationParameters>();
+    }
+    this.parameters.add(parametersItem);
+    return this;
+  }
+
+  /**
+   * Get parameters
+   * @return parameters
+  **/
+  @ApiModelProperty(value = "")
+      @Valid
+    public List<MaskValuesInputOperationParameters> getParameters() {
+    return parameters;
+  }
+
+  public void setParameters(List<MaskValuesInputOperationParameters> parameters) {
+    this.parameters = parameters;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MaskValuesInputOperation maskValuesInputOperation = (MaskValuesInputOperation) o;
+    return Objects.equals(this.id, maskValuesInputOperation.id) &&
+        Objects.equals(this.parameters, maskValuesInputOperation.parameters);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, parameters);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class MaskValuesInputOperation {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    parameters: ").append(toIndentedString(parameters)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ spring/src/main/java/com/accutive/maas/swagger/model/MaskValuesInputOperationParameters.java
@@ -0,0 +1,102 @@
+package com.accutive.maas.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * MaskValuesInputOperationParameters
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-07-29T23:00:54.754Z[GMT]")
+public class MaskValuesInputOperationParameters   {
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("value")
+  private String value = null;
+
+  public MaskValuesInputOperationParameters name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+  **/
+  @ApiModelProperty(example = "mask_link", value = "")
+  
+    public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public MaskValuesInputOperationParameters value(String value) {
+    this.value = value;
+    return this;
+  }
+
+  /**
+   * Get value
+   * @return value
+  **/
+  @ApiModelProperty(example = "myMaskLink", value = "")
+  
+    public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MaskValuesInputOperationParameters maskValuesInputOperationParameters = (MaskValuesInputOperationParameters) o;
+    return Objects.equals(this.name, maskValuesInputOperationParameters.name) &&
+        Objects.equals(this.value, maskValuesInputOperationParameters.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, value);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class MaskValuesInputOperationParameters {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    value: ").append(toIndentedString(value)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
